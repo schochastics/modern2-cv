561a3b874379e67c8003aed1c963aacd957bac00
szeller42 <s.zeller@posteo.net>
2023-09-29 21:27:30 +0200

    💄 adjust paragraph spacing

--- _extensions/modern2-cover-letter/para.lua
+++ _extensions/modern2-cover-letter/para.lua
@@ -1,5 +1,5 @@
 Para = function(para)
-    table.insert(para.content, 1, pandoc.RawInline('tex', "\\cvtext{%\n"))
-    table.insert(para.content, pandoc.RawInline('tex', "}\n"))
+    table.insert(para.content, 1, pandoc.RawInline('tex', ""))
+    table.insert(para.content, pandoc.RawInline('tex', "\\\\[4pt]"))
     return para
 end
